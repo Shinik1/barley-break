86d8cdc502eb101cebabee6d083551c789f9d092
Anastaisha18 <nastyvavilina@gmail.com>
2024-12-03 00:05:51 +0500

    Update print_pole.py

--- print_pole.py
+++ print_pole.py
@@ -4,7 +4,7 @@ def print_pole(pole):  #вывод поля
     for i in range(4):
         for j in range(4):
             if pole[i][j] == ' ':
-                print("| "+ str(pole[i][j])+" ", end = ' ')
+                print("| "+str(pole[i][j])+" ", end = ' ')
             elif int(pole[i][j])<10:
                 print("| "+str(pole[i][j])+" ", end = ' ')
             else:
