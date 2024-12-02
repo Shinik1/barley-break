63e3eacf17f3a19338df21e29ca5a9463ceeecc4
Shiniki <nsbatrakov@gmail.com>
2024-12-02 23:59:55 +0500

    Update Barley-break.py

--- Barley-break.py
+++ Barley-break.py
@@ -3,8 +3,8 @@ import numpy as np
 from prettytable import PrettyTable
 
 from fill_pole import fill_pole
+from print_pole import print_pole
 from game_mechanics import mexanika
 from main import main
-
 
 main()
