823e71c7cd74151f89fb3d8de69347fec5827a7a
Shiniki <nsbatrakov@gmail.com>
2024-12-03 10:36:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fill_pole import fill_pole
 from game_mechanics import mexanika
 from print_pole import print_pole
-from main import main
 
-
-main()
+ win_pole = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,' ']] #победное поле
+ pole = [[],[],[],[]] #поле (двумерный массив)
+ pole = fill_pole(pole)
+ print_pole(pole)
+ mexanika(pole, win_pole)
